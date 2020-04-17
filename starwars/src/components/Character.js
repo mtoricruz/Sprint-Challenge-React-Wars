dbca56d07f840ce9d3d7d9f48d4da4465770a6ee
toricruzmendiola <mtoricruz@gmail.com>
2020-04-17 09:35:07 -0700

    added styles for mvp. Going to try accessing deeper information from deeper apis

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Info = styled.h4`
+    &:hover {
+        color:red;
+        transition: all 0.6s ease-in-out;
+        font-size: 24px;
+    }
+    transition: all 0.5s ease-in-out;
+    background-color: black;
+    margin-right: 30%;
+    margin-left: 30%;
+`
 
 const Character = ({ name, height, mass, birth_year }) => {
   return(
     <div className='characters'>
         <h2>Name: {name}</h2>
-        <h4>Height: {height}cm</h4>
-        <h4>Weight: {mass}kg</h4>
-        <p>Age: {birth_year}</p>
+        <Info className="height">Height: {height}cm</Info>
+        <Info className="weight">Weight: {mass}kg</Info>
+        <Info className="age">Age: {birth_year}</Info>
     </div>  
   )
 }
--- starwars/src/components/CharacterCards.js
+++ starwars/src/components/CharacterCards.js
@@ -1,16 +1,34 @@
 // Write your Character component here
 import React from 'react';
 import Character from './Character'
+import styled from "styled-components"
+
+const ContentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const CharacterContainer = styled.div`
+    background-color: white;
+    opacity: 90%;
+    width: 40%;
+    padding-top: 3%;
+    padding-bottom: 3%;
+    margin: 2%;
+    border-radius: 20px;
+`
 
 const CharacterCards = ({ results }) => {
   return (
-    <div className="character-container">
-      {
-        results.map((result, id) => {
-          return <Character name={result.name} height={result.height} mass={result.mass} birth_year={result.birth_year} key={id}/>
-        })
-      }
-    </div>
+    <ContentContainer>  
+        <CharacterContainer className="character-container">
+        {
+            results.map((result, id) => {
+            return <Character name={result.name} height={result.height} mass={result.mass} birth_year={result.birth_year} key={id}/>
+            })
+        }
+        </CharacterContainer>
+    </ContentContainer>    
   );
 };
 
